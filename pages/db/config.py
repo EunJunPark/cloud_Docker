40d3f19a7d782a982458bccd7a641919bc5f4e4c
JunEunPark <pppp662667@gmail.com>
2025-03-14 16:45:04 +0900

    port

--- pages/db/config.py
+++ pages/db/config.py
@@ -5,4 +5,4 @@ class Config:
     MYSQL_USER = os.getenv('MYSQL_USER', 'myuser')  # root 대신 myuser 사용
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '1234')
     MYSQL_DB = os.getenv('MYSQL_DB', 'mydatabase')
-    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3307))
