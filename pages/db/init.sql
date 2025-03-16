5a7d93c9cd41a019d0490e543b1a3ea4eb8a41f5
JunEunPark <pppp662667@gmail.com>
2025-03-16 21:01:37 +0900

    init.sql modi

--- pages/db/init.sql
+++ pages/db/init.sql
@@ -2,15 +2,6 @@
 CREATE DATABASE IF NOT EXISTS mydatabase;
 USE mydatabase;
 
--- 1. 테이블 생성
-CREATE TABLE IF NOT EXISTS board (
-    idx INT AUTO_INCREMENT PRIMARY KEY,   
-    name VARCHAR(100) NOT NULL,          
-    pw VARCHAR(255) NOT NULL,             
-    context TEXT NOT NULL,                
-    currentstatus TINYINT DEFAULT 1       
-);
-
 -- 2. 기존 사용자 삭제 (이미 존재하는 경우)
 DROP USER IF EXISTS 'admin'@'%';
 
@@ -22,3 +13,13 @@ GRANT ALL PRIVILEGES ON mydatabases.* TO 'admin'@'%';
 
 -- 5. 변경 사항 적용
 FLUSH PRIVILEGES;
+
+-- 1. 테이블 생성
+CREATE TABLE IF NOT EXISTS board (
+    idx INT AUTO_INCREMENT PRIMARY KEY,   
+    name VARCHAR(100) NOT NULL,          
+    pw VARCHAR(255) NOT NULL,             
+    context TEXT NOT NULL,                
+    currentstatus TINYINT DEFAULT 1       
+);
+
